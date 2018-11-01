1f50ce7269b6cebb061a010173968bf016563c7b
qijiuyu79 <jiuyu.qi@edaibu.net>
2018-11-01 14:31:10 +0800

    开发逛街功能

--- app/src/main/java/com/youmai/project/activity/user/UserActivity.java
+++ app/src/main/java/com/youmai/project/activity/user/UserActivity.java
@@ -285,6 +285,7 @@ public class UserActivity extends BaseActivity implements View.OnClickListener{
      */
     private List<ImageView> imgList=new ArrayList<>();
     private void setXing(int index){
+        index=2;
         imgList.add(imgX1);
         imgList.add(imgX2);
         imgList.add(imgX3);
